e2353ca01e1cb6bd61d133360392312989f2d769
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-10-04 15:13:58 +0000

    fix: apply last syntax fixes and then some
    Signed-off-by: GitHub <noreply@github.com>

--- java/src/dev/andreijiroh/studentops/projects/variableNamingConventions.java
+++ java/src/dev/andreijiroh/studentops/projects/variableNamingConventions.java
@@ -5,24 +5,25 @@ package dev.andreijiroh.studentops.projects;
  * @author Andrei Jiroh Eugenio Halili (STI College Meycauayan) <halili.459491@meycauayan.sti.edu.ph>
  */
 public class variableNamingConventions {
-    public static void(String[] args) {
+    public static void main(String[] args) {
         // Set A
         int age = 24; // age
-        double height = 1.75 // in meters
+        double height = 1.75; // in meters
         String legalName = "Juan Luna IV"; // full name
-        char gender = 'M' // gender
+        char gender = 'M'; // gender
         boolean isStudent = true; // confirming if the person is a student
 
         // Set B but fixed
         int subjects = 6; // number of subjects
-        double weight = 70.5 // weight in kg
+        double weight = 70.5; // weight in kg
         String birth_place = "Ilocos Norte"; // birthplace
         char blood_type = 'O'; // blood type
         boolean has_pets = true; // confirming if the person has pets
 
         // Set C
         System.out.println("Name: "+legalName);
-        System.out.println("Height: "+height);
+        System.out.println("Age: "+age);
+        System.out.println("Height: "+height+" meters");
         System.out.println("Gender: "+gender);
         System.out.println("Is Student: "+isStudent);
         System.out.println("Number of Subjects: "+subjects);
