ab43bb7e02d796a815af3e1970aee9f57c4c8a80
Andrei Jiroh Halili <halili.459491@meycauayan.sti.edu.ph>
2025-11-29 19:21:39 +0800

    chore: refactor main method in finaPT class
    Gonna check for issues on a Remote Tunnel server later
    
    Signed-off-by: Andrei Jiroh Halili <halili.459491@meycauayan.sti.edu.ph>

--- java/src/dev/andreijiroh/studentops/projects/finaPT.java
+++ java/src/dev/andreijiroh/studentops/projects/finaPT.java
@@ -6,29 +6,26 @@ import module java.base;
  * Computer Programming 1 - 07 Performance Task 1
  * @author Andrei Jiroh Eugenio Halili (STI College Meycauayan) <halili.459491@meycauayan.sti.edu.ph>
  */
-public class finaPT {
-  public static void main(String[] args) {
-    // Output
-    System.out.println("Well hello there!");
+void main(String[] args) {
+  // Output
+  System.out.println("Well hello there!");
 
-    // Input + loops
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      System.out.print("What's cooking for today? ");
-      String dailyLog = scanner.nextLine().trim();
+  // Input + loops
+  Scanner scanner = new Scanner(System.in);
+  while (true) {
+    System.out.print("What's cooking for today? ");
+    String dailyLog = scanner.nextLine().trim();
 
-      if (dailyLog.isEmpty()) System.out.print("You forgot to log something");
+    if (dailyLog.isEmpty()) System.out.print("You forgot to log something");
 
-      break;
-    }
-    
-
-    // Array
-
-    // Conditionals (if-then-else, switch)
-
-    // loops
-
-    scanner.close();
+    break;
   }
+
+  // Array
+
+  // Conditionals (if-then-else, switch)
+
+  // loops
+
+  scanner.close();
 }
