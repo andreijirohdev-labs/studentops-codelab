fe81f9a2274bde19e74a1e0f40b847941c3c52ff
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-12-01 02:47:37 +0000

    chore: more chaos in finalPT code
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

--- java/src/dev/andreijiroh/studentops/projects/finaPT.java
+++ java/src/dev/andreijiroh/studentops/projects/finaPT.java
@@ -6,26 +6,27 @@ import module java.base;
  * Computer Programming 1 - 07 Performance Task 1
  * @author Andrei Jiroh Eugenio Halili (STI College Meycauayan) <halili.459491@meycauayan.sti.edu.ph>
  */
-void main(String[] args) {
-  // Output
-  System.out.println("Well hello there!");
+public class finaPT {
+  public static void main(String[] args) {
+    // Output
+    System.out.println("Well hello there!");
 
-  // Input + loops
-  Scanner scanner = new Scanner(System.in);
-  while (true) {
-    System.out.print("What's cooking for today? ");
-    String dailyLog = scanner.nextLine().trim();
+    // Input + loops
+    while (true) {
+      System.out.print("What's cooking for today? ");
+      String dailyLog = scanner.nextLine().trim();
+      Scanner scanner = new Scanner(System.in);
+      if (dailyLog.isEmpty()) {
+        System.out.println("You forgot to log something");
+      } else {
+        break;
+      }
+    }
 
-    if (dailyLog.isEmpty()) System.out.print("You forgot to log something");
+    // Array + conditionals (switches)
+    string[] categories = {"School", "Work", "Personal", "Others"};
+    println("What category is this?")
 
-    break;
+    scanner.close();
   }
-
-  // Array
-
-  // Conditionals (if-then-else, switch)
-
-  // loops
-
-  scanner.close();
 }
