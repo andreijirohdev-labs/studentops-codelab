437c39b27f061d0d348cafc9e29bbff3b50fd149
ajhalili2006 <ajhalili2006@gmail.com>
2026-02-16 02:36:29 +0000

    chore: get cooking finally at 02 Laboratory Exercise
    Signed-off-by: ajhalili2006 <ajhalili2006@gmail.com>

--- java/src/dev/andreijiroh/studentops/projects/ComProg2_Lab2_v1.java
+++ java/src/dev/andreijiroh/studentops/projects/ComProg2_Lab2_v1.java
@@ -2,52 +2,59 @@ package dev.andreijiroh.studentops.projects;
 
 /**
  * Computer Programming 2 - 02 Laboratory Exercise
+ * v1 - Initial version
  * @author Andrei Jiroh Halili (STI College Meycauayan) <halili.459491@meycauayan.sti.edu.ph>
  */
-interface Self {
-  void introduce();
-};
+class ComProg2_Lab2_v1 {
+  /**
+   * Our basic interface for introducing ourselves
+   */
+  interface Self {
+    void introduce();
+  };
 
-class Halili implements Self {
-  int age;
-  String[] interests;
-  String[] skills;
+  /**
+   * The extender part of the initial interface
+   */
+  interface Self_addon extends Self {
+    void webAndSocials();
+  }
 
-  public void introduce() {
-    this.age = age;
-    this.interests = interests;
-    this.skills = skills;
+  static class Halili implements Self {
+    int age;
+    String[] interests;
+    String[] skills;
 
-    System.out.println("Age: "+age);
-    System.out.println("Interests");
-    for (int i = 0; i < interests.length; i++) {
-      System.out.println("- "+interests[i]);
-    }
-    System.out.println("Skills");
-    for (int i = 0; i < skills.length; i++) {
-      System.out.println("- "+skills[i]);
+    public void introduce() {
+      System.out.println("Age: "+age);
+      System.out.println("Interests");
+      for (int i = 0; i < interests.length; i++) {
+        System.out.println("- "+interests[i]);
+      }
+      System.out.println("Skills");
+      for (int i = 0; i < skills.length; i++) {
+        System.out.println("- "+skills[i]);
+      }
+    };
+  }
+
+  static class AndreiJiroh extends Halili implements Self_addon {
+    String website;
+    String[] socials;
+
+    public void webAndSocials() {
+      System.out.println("Website: "+website);
+      System.out.println("Socials:");
+      for (int i = 0; i < socials.length; i++) {
+        System.out.println("- "+socials[i]);
+      }
     }
   };
-}
 
-class AndreiJiroh extends Halili {
-  String website;
-  String[] socials;
-
-  public void webAndSocials() {
-    System.out.println("Website: "+website);
-    System.out.println("Socials:");
-    for (int i = 0; i < socials.length; i++) {
-      System.out.println("- "+socials[i]);
-    }
-  }
-};
-
-class ComProg2_Lab2_v1 {
   public static void main(String[] args) {
     int age = 20;
     String[] interests = {"Nixpkgs/Nix/NixOS","electronic music","Minecraft"};
-    String[] skills = {"Backend development in TypeScript", "Linux shell scripting"};
+    String[] skills = {"Backend development in TypeScript", "Linux shell scripting", "Technical documentation writing", "DevOps with Docker and GitHub Actions + GitLab Ci"};
     String website = "https://andreijiroh.dev";
     String[] socials = {"https://tilde.zone/@ajhalili2006", "https://bsky.app/profile/andreijiroh.dev", "https://instagram.com/ajhalili2006", "https://youtube.com/@ajhalili2006", "https://x.com/ajhalili2006"};
     
