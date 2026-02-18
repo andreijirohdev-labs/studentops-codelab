b9181691807569c93ad07c4e56a7998bced547d4
ajhalili2006 <ajhalili2006@gmail.com>
2026-02-18 23:45:29 +0000

    commit the thing for now, do work otg via termux
    Signed-off-by: ajhalili2006 <ajhalili2006@gmail.com>

---
+++ java/src/dev/andreijiroh/studentops/projects/ComProg2_Lab3_p1.java
@@ -0,0 +1,16 @@
+package dev.andreijiroh.studentops.projects;
+import java.util.Scanner;
+
+/**
+ * Computer Programming 2 - 03 Laboratory Exercise 1
+ * @author Andrei Jiroh Halili (STI College Meycauayan) <halili.459491@meycauayan.sti.edu.ph>
+ */
+public class ComProg2_Lab3_p1 {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    // TODO: Implementation here
+
+    input.close();
+  }
+}
